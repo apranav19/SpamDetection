77cd743fb5c2d63b694d8ef33bf7e96b20629d71
PranavAngara19 <pranavangara19@ubuntu.(none)>
2014-03-04 18:51:58 -0800

    Add indicator file & implement gradient descent

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import math
 import os
 
 '''
@@ -7,30 +8,65 @@ import os
 	(x - mean)/std_deviation
 '''
 def standardize(matrix):
-	rows = len(matrix)
-	cols = len(matrix[0])
-
 	feature_averages = matrix.mean(axis=0)	# Unidimensional array containing means for each column
 	feature_std_devs = matrix.std(axis=0)	# Unidimensional array containing std_devs for each column
 
-	for i in xrange(0, rows):
-		for j in xrange(0, cols):
+	for i, row in enumerate(matrix):
+		for j, col in enumerate(matrix[i]):
 			standardized_val = (matrix[i, j] - feature_averages[j])/(feature_std_devs[j]) # Compute z-score
 			matrix[i, j] = standardized_val # Set element to its computed z-score
 
 	return matrix
 
 
-if __name__ == '__main__':
-	data_file = os.getcwd() + "/spam.data"
-	matrix = np.genfromtxt(data_file)[:3065, :57] # For this experiment, I'm using this as my training data
+def sigmoid(instance, weight_instance):
+	denom = 1.0 + math.exp((-1 * np.dot(weight_instance.T, instance)))
+	return 1/denom
+
+def gradient_descent(learning_rate, input_matrix, results, weights):
+	while convergence <= 0.01: 
+		for j in xrange(0, len(weights)):
+			sum_likelihood = 0
+			for i in xrange(0, len(input_matrix)):
+				sum_likelihood += (sigmoid(input_matrix[i], weights) - results[i]) * input_matrix[i][j]
+
+			weights[j] = weights[j] - (learning_rate * sum_likelihood)
+			convergence = weights[j] - weights[j-1]
 	
-	standardized_matrix = standardize(matrix)
+	return weights
 
-	# Check that mean is about 0
-	print standardized_matrix.mean(axis=0)
-	print "\n"
+def fill_bias_features(input_matrix):
+	return np.insert(input_matrix, 0, 1, axis=1)
 
-	# Check that unit variance exists
-	print standardized_matrix.std(axis=0)
-	print "\n"
+
+def init_files():
+	f1 = os.getcwd() + "/spam.traintest.txt"
+	f2 = os.getcwd() + "/spam.data"
+
+	return f1, f2
+
+def filter_data(indication_matrix, data_matrix):
+	training_data = [data_matrix[i] for i, j in enumerate(indication_matrix) if j == 0]
+	test_data = [data_matrix[i] for i, j in enumerate(indication_matrix) if j == 1]
+
+	return np.asarray(training_data), np.asarray(test_data)
+
+def extract_results(training_data, test_data):
+	return training_data[:, 57:], test_data[:, 57:]
+
+
+if __name__ == '__main__':
+	indicator_file, data_file = init_files()
+
+	indication_matrix = np.genfromtxt(indicator_file)
+
+	data_matrix = np.genfromtxt(data_file)
+	training_matrix, test_matrix = filter_data(indication_matrix, data_matrix)
+
+	training_results, test_results = extract_results(training_matrix, test_matrix)
+
+	training_matrix = training_matrix[:, :57]
+	test_matrix = test_matrix[:, :57]
+
+	print "Dimension of training: " + str(training_results.shape) + " \n"
+	print " Dimension of test: " + str(test_results.shape)

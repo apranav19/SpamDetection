2ba264986aea05ac9a58071da0a75b45c87897dc
PranavAngara19 <pranavangara19@ubuntu.(none)>
2014-03-04 22:59:57 -0800

    Add MSE & test the model

--- spam_classifier.py
+++ spam_classifier.py
@@ -19,21 +19,39 @@ def standardize(matrix):
 	return matrix
 
 
-def sigmoid(instance, weight_instance):
-	denom = 1.0 + math.exp((-1 * np.dot(weight_instance.T, instance)))
+def sigmoid(instance, weights):
+	dot_prod = np.dot(weights.T, instance)
+	exp_val = math.e ** (-1 * dot_prod)
+	denom = 1.0 + exp_val
+
 	return 1/denom
 
 def gradient_descent(learning_rate, input_matrix, results, weights):
-	while convergence <= 0.01: 
-		for j in xrange(0, len(weights)):
-			sum_likelihood = 0
-			for i in xrange(0, len(input_matrix)):
-				sum_likelihood += (sigmoid(input_matrix[i], weights) - results[i]) * input_matrix[i][j]
+	temp_weights = np.copy(weights)
 
-			weights[j] = weights[j] - (learning_rate * sum_likelihood)
-			convergence = weights[j] - weights[j-1]
+	for j in xrange(0, len(weights)):
+		sum_likelihood = 0
+		for i in xrange(0, len(input_matrix)):
+			sum_likelihood += (sigmoid(input_matrix[i], weights) - results[i]) * input_matrix[i][j]
+
+		weights[j] = weights[j] - (learning_rate * sum_likelihood)
+
+		if math.fabs(weights[j] - temp_weights[j]) < 0.01:
+			return weights
 	
 	return weights
+
+def stochastic_gradient_descent(input_matrix, learning_rate):
+	rows, cols = np.shape(input_matrix)
+	theta = np.random.random(cols)
+	z = np.arange(rows)
+
+	for i in xrange(0, 1000):
+		z = np.random.permutation(z)
+		for idx in z:
+			theta = theta - (learning_rate * sigmoid(input_matrix[idx], theta) * input_matrix[idx])
+
+	return theta
 
 def fill_bias_features(input_matrix):
 	return np.insert(input_matrix, 0, 1, axis=1)
@@ -65,8 +83,26 @@ if __name__ == '__main__':
 
 	training_results, test_results = extract_results(training_matrix, test_matrix)
 
-	training_matrix = training_matrix[:, :57]
-	test_matrix = test_matrix[:, :57]
+	training_matrix = standardize(training_matrix)[:, :57]
+	test_matrix = standardize(test_matrix)[:, :57]
 
-	print "Dimension of training: " + str(training_results.shape) + " \n"
-	print " Dimension of test: " + str(test_results.shape)
+	weights = np.ones((57, 1))
+	gen_weights = gradient_descent(0.01, training_matrix, training_results, weights)
+	my_res = []
+
+	probs = [sigmoid(instance, gen_weights) for instance in test_matrix]
+	for prob in probs:
+		if prob >= (1.0 - prob):
+			my_res.append(1)
+		else:
+			my_res.append(0)
+
+	err = sum([((my_res[idx] - test_results[idx]) ** 2)for idx in xrange(0, len(my_res))])
+
+	print "MAE: " + str(err/len(my_res)) + "\n" 
+
+	print "Spam: " + str(my_res.count(1)) + " Not Spam: " + str(my_res.count(0))
+
+
+	#print "Dimension of training: " + str(training_results.shape) + " \n"
+	#print " Dimension of test: " + str(test_results.shape)

fb97653f661269c9484c2a29d11b31cc0ab007f8
PranavAngara19 <pranavangara19@ubuntu.(none)>
2014-03-05 16:17:01 -0800

    Replace custom algorithm with SciKit

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,4 +1,7 @@
-import numpy as np 
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+import sklearn.metrics as sk_metrics
+import numpy as np
 import math
 import os
 
@@ -8,101 +11,75 @@ import os
 	(x - mean)/std_deviation
 '''
 def standardize(matrix):
-	feature_averages = matrix.mean(axis=0)	# Unidimensional array containing means for each column
-	feature_std_devs = matrix.std(axis=0)	# Unidimensional array containing std_devs for each column
+	preprocessed_data = preprocessing.scale(matrix)
+	return preprocessed_data
 
-	for i, row in enumerate(matrix):
-		for j, col in enumerate(matrix[i]):
-			standardized_val = (matrix[i, j] - feature_averages[j])/(feature_std_devs[j]) # Compute z-score
-			matrix[i, j] = standardized_val # Set element to its computed z-score
+def binarize(matrix):
+	preprocessed_data = preprocessing.binarize(matrix)
+	return preprocessed_data
 
+def log_map(elem):
+	transform_const = 0.1
+	return math.log(elem + 0.1)
+
+def log_transform(matrix):
+	for i, instance in enumerate(matrix):
+		matrix[i] = map(log_map, instance)
 	return matrix
-
-
-def sigmoid(instance, weights):
-	dot_prod = np.dot(weights.T, instance)
-	exp_val = math.e ** (-1 * dot_prod)
-	denom = 1.0 + exp_val
-
-	return 1/denom
-
-def gradient_descent(learning_rate, input_matrix, results, weights):
-	temp_weights = np.copy(weights)
-
-	for j in xrange(0, len(weights)):
-		sum_likelihood = 0
-		for i in xrange(0, len(input_matrix)):
-			sum_likelihood += (sigmoid(input_matrix[i], weights) - results[i]) * input_matrix[i][j]
-
-		weights[j] = weights[j] - (learning_rate * sum_likelihood)
-
-		if math.fabs(weights[j] - temp_weights[j]) < 0.01:
-			return weights
-	
-	return weights
-
-def stochastic_gradient_descent(input_matrix, learning_rate):
-	rows, cols = np.shape(input_matrix)
-	theta = np.random.random(cols)
-	z = np.arange(rows)
-
-	for i in xrange(0, 1000):
-		z = np.random.permutation(z)
-		for idx in z:
-			theta = theta - (learning_rate * sigmoid(input_matrix[idx], theta) * input_matrix[idx])
-
-	return theta
-
-def fill_bias_features(input_matrix):
-	return np.insert(input_matrix, 0, 1, axis=1)
-
 
 def init_files():
 	f1 = os.getcwd() + "/spam.traintest.txt"
 	f2 = os.getcwd() + "/spam.data"
 
 	return f1, f2
-
+'''
+	Using the indicator file, this function filters out the training data & test data from the 
+	raw input file
+'''
 def filter_data(indication_matrix, data_matrix):
 	training_data = [data_matrix[i] for i, j in enumerate(indication_matrix) if j == 0]
 	test_data = [data_matrix[i] for i, j in enumerate(indication_matrix) if j == 1]
-
 	return np.asarray(training_data), np.asarray(test_data)
 
 def extract_results(training_data, test_data):
 	return training_data[:, 57:], test_data[:, 57:]
 
+def calculate_MAE(calculated_results, actual_results):
+	error = sum([((calculated_results[idx] - actual_results[idx]) ** 2) for idx in xrange(0, len(calculated_results))])
+
+	return error/len(calculated_results)
+
+def init_regression_model(training_matrix, training_results):
+	regression_model = LogisticRegression()
+	regression_model.fit(training_matrix, training_results)
+
+	return regression_model
+
+def predict_results(test_data, regression_model):
+	return regression_model.predict(test_data)
 
 if __name__ == '__main__':
+	'''
+		Begin Dataset processing 
+	'''
 	indicator_file, data_file = init_files()
-
 	indication_matrix = np.genfromtxt(indicator_file)
 
 	data_matrix = np.genfromtxt(data_file)
 	training_matrix, test_matrix = filter_data(indication_matrix, data_matrix)
-
 	training_results, test_results = extract_results(training_matrix, test_matrix)
 
-	training_matrix = standardize(training_matrix)[:, :57]
-	test_matrix = standardize(test_matrix)[:, :57]
+	#training_matrix = binarize(training_matrix[:, :57])
+	#test_matrix = binarize(test_matrix[:, :57])
 
-	weights = np.ones((57, 1))
-	gen_weights = gradient_descent(0.01, training_matrix, training_results, weights)
-	my_res = []
+	training_matrix = log_transform(training_matrix[:, :57])
+	test_matrix = log_transform(test_matrix[:, :57])
 
-	probs = [sigmoid(instance, gen_weights) for instance in test_matrix]
-	for prob in probs:
-		if prob >= (1.0 - prob):
-			my_res.append(1)
-		else:
-			my_res.append(0)
+	'''
+		End of Dataset processing
+	'''
 
-	err = sum([((my_res[idx] - test_results[idx]) ** 2)for idx in xrange(0, len(my_res))])
+	log_regresson_model = init_regression_model(training_matrix, training_results)
+	my_results = log_regresson_model.predict(training_matrix)
 
-	print "MAE: " + str(err/len(my_res)) + "\n" 
-
-	print "Spam: " + str(my_res.count(1)) + " Not Spam: " + str(my_res.count(0))
-
-
-	#print "Dimension of training: " + str(training_results.shape) + " \n"
-	#print " Dimension of test: " + str(test_results.shape)
+	print sk_metrics.mean_squared_error(training_results, my_results)
